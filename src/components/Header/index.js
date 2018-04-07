fdd9c2f3c86e5f908df84ac71d2957395c867ef0
Stefan Ivic <nightwalkerkg@gmail.com>
2018-04-07 14:37:00 +0200

    styled: implements basic styles for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,28 +3,59 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 const MainHeader = styled.section`
-	background: rebeccapurple;
+	background: white;
 	margin-bottom: 1.45rem;
+	.sticky {
+		width: 100%;
+		position: fixed;
+		top: 0;
+		z-index: 1;
+	}
 `
 
 const Wrapper = styled.div`
 	margin: 0 auto;
 	max-width: 1200px;
 	padding: 1.45rem 1.0875rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 `
 
 const Logo = styled.a`
-	color: white;
+	color: black;
 	text-decoration: none;
 	margin-bottom: 0;
 	font-weight: bold;
 	font-size: 2.25rem;
 `
 
+const Navigation = styled.ul`
+	list-style: none;
+	margin: 0;
+	display: flex;
+`
+
+const NavigationItem = styled.li`
+	margin: 0 10px;
+`
+
+const NavigationLink = styled.a`
+	color: black;
+`
+
 const Header = () => (
 	<MainHeader>
 		<Wrapper>
-			<Logo href="/">Gatsby</Logo>
+			<Logo href="/">si.</Logo>
+			<Navigation>
+				<NavigationItem>
+					<NavigationLink href="/about">About</NavigationLink>
+				</NavigationItem>
+				<NavigationItem>
+					<NavigationLink href="/blog">Blog</NavigationLink>
+				</NavigationItem>
+			</Navigation>
 		</Wrapper>
 	</MainHeader>
 )
