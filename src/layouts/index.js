1f03321106510b8b844753b20a58f9062f6b1c64
Stefan Ivic <nightwalkerkg@gmail.com>
2018-07-01 22:22:32 +0200

    styling: change selected text color based on website

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import favIcon from '../assets/favicon.png'
+import css from '../assets/app.css'
 
 import Header from '../components/Header'
 
