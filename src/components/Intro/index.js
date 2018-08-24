b8c4fd19f7b147160cd385d95752bd638755c4e2
Stefan Ivic <nightwalkerkg@gmail.com>
2018-08-24 02:00:00 +0200

    typo: fix a typo

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -11,12 +11,12 @@ const Intro = () => (
         TECH AND SCIENCE ENTHUSIAST.
       </IntroSection.SubHeading>
       <Common.Text>
-        I am a Web Developer with a strong focus on performance and
-        scale-ability of web applications. I love utilizing User Experience
-        research to craft good designs and apply them with code. I take part in
-        the open source community where I can use the latest technologies and
-        best practices. I write blog posts and my latest interest is in helping
-        beginners learn more about the web.
+        I am a Web Developer with a strong focus on performance and scalability
+        of web applications. I love utilizing User Experience research to craft
+        good designs and apply them with code. I take part in the open source
+        community where I can use the latest technologies and best practices. I
+        write blog posts and my latest interest is in helping beginners learn
+        more about the web.
       </Common.Text>
       <Links to="/about">Read the full bio</Links>
     </IntroSection.Wrapper>
